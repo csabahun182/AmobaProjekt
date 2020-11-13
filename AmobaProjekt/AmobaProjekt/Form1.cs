14d3aa5cd946f9f55a842c70e31b592286a35492
csabahun182 <46059281+csabahun182@users.noreply.github.com>
2020-11-13 18:42:45 +0100

    1.01 or something
    This version has 2 forms I suppose, you can click on one of those. Got some labels too!

---
+++ AmobaProjekt/AmobaProjekt/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmobaProjekt
+{ 
+
+    public partial class Form1 : Form
+    {
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+           //Ez a gomb átváltja magát a Form2-re, ami a jatekForm megnevezést kapta.
+            using (var form2 = new jatekForm())
+            {
+                form2.ShowDialog();
+                
+            }
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+    }
+}
---
+++ AmobaProjekt/AmobaProjekt/jatekForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmobaProjekt
+{
+    public partial class jatekForm : Form
+    {
+        public jatekForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+           
+            
+        }
+    }
+}
